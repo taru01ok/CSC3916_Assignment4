b7f2c42109df3b6319bba5165c2355d6c38c9dcd
Taruni Sabhavat <taruni.sabhavat@ucdenver.edu>
2026-04-17 20:56:33 -0600

    Fix auth_jwt for Mongoose 8 compatibility

--- auth_jwt.js
+++ auth_jwt.js
@@ -1,22 +1,26 @@
-require('dotenv').config();
+node -e "
+const fs = require('fs');
+const content = \`require('dotenv').config();
 var passport = require('passport');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var User = require('./Users');
 
 var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
 opts.secretOrKey = process.env.SECRET_KEY;
 
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findById(jwt_payload.id, function (err, user) {
-        if (user) {
-            done(null, user);
-        } else {
-            done(null, false);
-        }
-    });
+passport.use(new JwtStrategy(opts, async function(jwt_payload, done) {
+    try {
+        const user = await User.findById(jwt_payload.id);
+        if (user) { done(null, user); } else { done(null, false); }
+    } catch (err) { done(err, false); }
 }));
 
-exports.isAuthenticated = passport.authenticate('jwt', { session : false });
-exports.secret = opts.secretOrKey ;
+exports.isAuthenticated = passport.authenticate('jwt', { session: false });
+exports.secret = opts.secretOrKey;\`;
+fs.writeFileSync('auth_jwt.js', content);
+console.log('Done!');
+"
+
+
